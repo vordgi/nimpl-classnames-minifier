1d1f87038da69689ba21880492ee8b9f977cad7f
Vordgi <sasha2822222@gmail.com>
2021-12-29 15:16:28 +0200

    NCM-3 refactor classnames minifier

--- src/ClassnamesMinifier.ts
+++ src/ClassnamesMinifier.ts
@@ -2,29 +2,42 @@ import type { LoaderContext } from 'webpack/types';
 
 class ClassMinifier {
   cache: {[resource: string]: {[className: string]: string}} = {};
-  symbols: string[] = [];
+
+  symbols: string[] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+  ];
+
   lastIndex = 0;
 
-  constructor() {
-    this.symbols = [...new Array(26)].reduce((acc, _e, i) => {
-      acc.push(String.fromCharCode(97 + i));
-      acc.push(String.fromCharCode(65 + i));
-      return acc;
-    }, []);
-  }
+  nextLoopEndsWith = 26;
+
+  currentLoopLength = 0;
+
+  nameMap = [0];
 
   getClassName() {
-    const symbolsCount = 52;
-    let offset = this.lastIndex;
-    let className = '';
-
-    while (offset !== 0 || !className) {
-      const currentSymbolIndex = offset % symbolsCount;
-      className = this.symbols[currentSymbolIndex] + className;
-      offset = Math.floor(offset / symbolsCount);
+    const symbolsCount = 62;
+    if (this.lastIndex >= this.nextLoopEndsWith) {
+      if (this.nextLoopEndsWith === 26) this.nextLoopEndsWith = 62 * symbolsCount;
+      else this.nextLoopEndsWith = this.nextLoopEndsWith * symbolsCount;
+      this.nameMap.push(0);
+      this.currentLoopLength += 1
     }
 
-    return className;
+    const currentClassname = this.nameMap.map((e) => this.symbols[e]).join('');
+
+    for (let i = this.currentLoopLength; i >= 0; i--) {
+      if (this.nameMap[i] === symbolsCount - 1 || (i === 0 && this.nameMap[i] === 25)) {
+        this.nameMap[i] = 0;
+      } else {
+        this.nameMap[i] += 1;
+        break;
+      }
+    }
+
+    return currentClassname;
   }
 
   getLocalIdent({ resourcePath }: LoaderContext<any>, _localIdent: string, origName: string) {
