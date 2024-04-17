ed9317a6fbad480006905b17532f316b21b198dc
Vordgi <sasha2822222@gmail.com>
2024-04-17 21:30:32 +0400

    ncm-72 format files by eslint

--- src/lib/injectConfig.ts
+++ src/lib/injectConfig.ts
@@ -1,30 +1,25 @@
-import type { RuleSetUseItem, ModuleOptions } from 'webpack';
-import type ClassnamesMinifier from 'classnames-minifier';
+import type { RuleSetUseItem, ModuleOptions } from "webpack";
+import type ClassnamesMinifier from "classnames-minifier";
 
 export type InjectConfig = {
     localIdentName?: string;
     classnamesMinifier: ClassnamesMinifier;
-}
+};
 
-const injectConfig = (config: InjectConfig, rules?: ModuleOptions['rules']) => {
+const injectConfig = (config: InjectConfig, rules?: ModuleOptions["rules"]) => {
     if (!rules) return;
 
-    const oneOfRule = rules?.find(
-        (rule) => typeof rule === 'object' && typeof rule?.oneOf === 'object'
-    );
+    const oneOfRule = rules?.find((rule) => typeof rule === "object" && typeof rule?.oneOf === "object");
 
-    if (oneOfRule && typeof oneOfRule === 'object') {
-        const testCssLoaderWithModules = (loaderObj: RuleSetUseItem) => (
-            typeof loaderObj === 'object' &&
-            loaderObj?.loader?.match('css-loader') &&
-            typeof loaderObj.options === 'object' &&
-            loaderObj.options.modules
-        );
-        const testFontLoader = (loaderObj: RuleSetUseItem) => (
-            typeof loaderObj === 'object' &&
-            loaderObj?.loader?.match('next-font-loader')
-        );
-        oneOfRule.oneOf?.forEach(rule => {
+    if (oneOfRule && typeof oneOfRule === "object") {
+        const testCssLoaderWithModules = (loaderObj: RuleSetUseItem) =>
+            typeof loaderObj === "object" &&
+            loaderObj?.loader?.match("css-loader") &&
+            typeof loaderObj.options === "object" &&
+            loaderObj.options.modules;
+        const testFontLoader = (loaderObj: RuleSetUseItem) =>
+            typeof loaderObj === "object" && loaderObj?.loader?.match("next-font-loader");
+        oneOfRule.oneOf?.forEach((rule) => {
             if (rule && Array.isArray(rule.use)) {
                 let cssLoaderIndex = null as null | number;
                 for (let i = rule.use.length - 1; i >= 0; i--) {
@@ -39,7 +34,7 @@ const injectConfig = (config: InjectConfig, rules?: ModuleOptions['rules']) => {
                     if (testFontLoader(loaderObj)) break;
 
                     if (testCssLoaderWithModules(loaderObj)) {
-                        if (typeof loaderObj !== 'object' || typeof loaderObj.options !== 'object') continue;
+                        if (typeof loaderObj !== "object" || typeof loaderObj.options !== "object") continue;
                         cssLoaderIndex = i;
                         loaderObj.options.modules.getLocalIdent = config.classnamesMinifier.getLocalIdent;
                     }
@@ -51,12 +46,12 @@ const injectConfig = (config: InjectConfig, rules?: ModuleOptions['rules']) => {
                         1,
                         config.classnamesMinifier.postLoader,
                         rule.use[cssLoaderIndex],
-                        config.classnamesMinifier.preLoader
-                    )
+                        config.classnamesMinifier.preLoader,
+                    );
                 }
             }
         });
     }
-}
+};
 
 export default injectConfig;
